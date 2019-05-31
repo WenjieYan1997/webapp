7c11b122982796eaf166d8f9e436769b38462d7e
WenjieYan1997 <yanwenjie@std.uestc.edu.cn>
2019-05-31 16:00:41 +0800

    second

--- www/test.py
+++ www/test.py
@@ -5,7 +5,7 @@ from models import User, Blog, Comment
 
 async def test(loop):
     await orm.create_pool(loop=loop, user='root', password='password', db='awesome')
-    u = User(name='Test', email='test@qq.com', passwd='1234567890', image='about:blank')
+    u = User(name='Test', email='test@qq.com', passwd='12345678', image='about:blank')
     await u.save()
     orm.__pool.close()
     await orm.__pool.wait_closed()
